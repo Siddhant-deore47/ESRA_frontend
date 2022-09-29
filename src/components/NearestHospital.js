9099928d6667b6f3f7963319a6cccabbb367490e
Siddhant-deore47 <siddhantdeore47@gmail.com>
2022-09-29 20:48:47 +0530

    NearestHospital

--- src/components/NearestHospital.js
+++ src/components/NearestHospital.js
@@ -7,7 +7,7 @@ import Footer from "./Footer";
 function NearestHospital() {
   const navigate = useNavigate();
   const resetData = () => {
-    localStorage.removeItem("nearestHospital");
+    localStorage.clear();
     navigate("/reportaccident");
   };
   return (
